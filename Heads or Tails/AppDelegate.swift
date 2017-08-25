979cc1dd1c95b7e94f5a3249b80ed357b6767252
amanketebo <amanuelketebo@yahoo.com>
2017-08-24 23:19:12 -0700

    Initial commit

---
+++ Heads or Tails/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Heads or Tails
+//
+//  Created by Amanuel Ketebo on 8/24/17.
+//  Copyright © 2017 Amanuel Ketebo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+}
+
---
+++ Heads or Tails/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Heads or Tails
+//
+//  Created by Amanuel Ketebo on 8/24/17.
+//  Copyright © 2017 Amanuel Ketebo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
