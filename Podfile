8e2be0e4f32f5f3c620fc5b26d90d65e6c462683
amanketebo <amanuelketebo@yahoo.com>
2017-08-25 18:36:46 -0700

    Add Firebase dependencies
    * Includes Core, Auth, and Database

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Heads or Tails' do
+	pod 'Firebase/Core'
+	pod 'Firebase/Auth'
+	pod 'Firebase/Database'
+end
---
+++ Pods/Target Support Files/Pods-Heads or Tails/Pods-Heads or Tails-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Heads_or_Tails : NSObject
+@end
+@implementation PodsDummy_Pods_Heads_or_Tails
+@end
