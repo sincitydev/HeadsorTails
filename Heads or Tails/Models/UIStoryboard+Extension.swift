7aa18b5e8053b2415637895b2a8a3cfd46e7e578
amanketebo <amanuelketebo@yahoo.com>
2017-08-29 17:48:21 -0700

    Create launch screen design

---
+++ Heads or Tails/Models/UIStoryboard+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIStoryboard+Extension.swift
+//  Heads or Tails
+//
+//  Created by Amanuel Ketebo on 8/29/17.
+//  Copyright © 2017 Amanuel Ketebo. All rights reserved.
+//
+
+import Foundation
