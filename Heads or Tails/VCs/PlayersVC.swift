f6c21f90d6220dcc1f5cfe5ad411cae832f24578
amanketebo <amanuelketebo@yahoo.com>
2017-08-30 21:35:59 -0700

    Add logout bar button
    * Remove unnecessary assets
    * Resize logout asset

--- Heads or Tails/VCs/PlayersVC.swift
+++ Heads or Tails/VCs/PlayersVC.swift
@@ -13,5 +13,20 @@ class PlayersVC: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews()
+    {
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.29, green:0.56, blue:0.89, alpha:1.0)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "logout"), style: .plain, target: self, action: #selector(logout))
+    }
+    
+    @objc private func logout()
+    {
+        print("YOOOO\n\n\n\n")
     }
 }
